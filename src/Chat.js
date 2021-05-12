e33692c4ed0a08dfbce68f06e34fdb226c519a54
Adhiti <shaili.adhiti@gmail.com>
2021-05-13 02:37:17 +0530

    Animate

--- src/Chat.js
+++ src/Chat.js
@@ -1,16 +1,54 @@
 import { IconButton } from "@material-ui/core";
 import MicNoneIcon from "@material-ui/icons/MicNone";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 import "./Chat.css";
+import { selectChatId, selectChatName } from "./features/chatSlice";
+import db from "./firebase";
 import Message from "./Message";
+import firebase from 'firebase';
+import { selectUser } from "./features/userSlice";
+import FlipMove from "react-flip-move";
 
 function Chat() {
 
+    const user = useSelector(selectUser);
     const [input, setInput] = useState("");
+    const chatName = useSelector(selectChatName);
+    const chatId = useSelector(selectChatId);
+    const [messages, setMessages] = useState([]);
+
+    useEffect(() => {
+        if(chatId) {
+            db.collection("chats")
+            .doc(chatId)
+            .collection("messages")
+            .orderBy("timestamp", "desc")
+            .onSnapshot((snapshot) =>
+            setMessages(
+                snapshot.docs.map((doc) =>({
+                    id: doc.id,
+                    data: doc.data(),
+                }))
+            )
+          );
+        }
+    }, [chatId]);
+
+
 
     const sendMessage = (e) => {
         e.preventDefault();
 
+        db.collection("chats").doc(chatId).collection("messages").add({
+            timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+            message: input,
+            uid: user.uid,
+            photo: user.photo,
+            email: user.email,
+            displayName: user.displayName,
+
+        });
         setInput("");
     }; 
     return (
@@ -18,17 +56,20 @@ function Chat() {
     {/* Chat header */}
     <div className="chat_header">
         <h4>
-        To: <span className="chat_name">Channel Name</span>
+        To: <span className="chat_name">{chatName}</span>
         </h4>
         <strong> Details</strong>
     </div>
 
     {/* Chat messages */}
     <div className="chat_messages">
-    <Message />
-    <Message />
-    <Message />
-        
+    <FlipMove>
+       {messages.map(({ id, data}) => (
+         <Message key={id} contents={data} />
+       ))}
+    </FlipMove>
+    
+  
     </div>
 
     {/* Chat input */}
